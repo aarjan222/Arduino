a6f42ef77183ee5a4e879599f03d680d7cefeb56
aarjanbu <077bei004.aarjan@pcampus.edu.np>
2022-11-16 08:32:32 +0545

    Code For BT Communication

---
+++ bluetooth/bluetooth.ino
@@ -0,0 +1,14 @@
+#include <SoftwareSerial.h>
+
+const int bt_en = 8, bt_vcc = 9, bt_gnd = 10, bt_tx = 11, bt_rx = 12, bt_state = 13;
+
+void setup() {
+  pinMode(bt_en, OUTPUT);
+  pinMode(bt_vcc, OUTPUT);
+  pinMode(bt_gnd, OUTPUT);
+  digitalWrite(bt_en, HIGH);
+}
+void loop() {
+
+digit
+}
