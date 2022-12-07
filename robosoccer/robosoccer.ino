9b9615ec384dc2f21aad33565948347c2d919e72
aarjanbu <077bei004.aarjan@pcampus.edu.np>
2022-12-07 22:38:45 +0545

    Code For Robosoccer

---
+++ robosoccer/robosoccer.ino
@@ -0,0 +1,67 @@
+#include <SoftwareSerial.h>
+
+const int en1 = 5, en2 = 6, in1 = 4, in2 = 7, in3 = 8, in4 = 9, statePin = 4;
+char c = ' ';
+SoftwareSerial myBT(0, 1);  //0 as rx and 1 ax tx
+
+void setup() {
+  Serial.begin(9600);
+  myBT.begin(115200);
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(statePin, INPUT);
+}
+
+void loop() {
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);
+  if (myBT.available() > 0) {
+    c = myBT.read();
+    switch (c) {
+      case 'F':
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        break;
+
+      case 'L':
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        break;
+
+      case 'R':
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        break;
+
+      case 'B':
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        break;
+
+      case 'S':
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, LOW);
+        break;
+    }
+  }
+  if (statePin == LOW) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
+}
