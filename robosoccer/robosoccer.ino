0ca75618dc74fec58cee2464ca90de012ed4d223
aarjanbu <077bei004.aarjan@pcampus.edu.np>
2022-12-08 00:09:54 +0545

    Code For Bt statepin and all done

--- robosoccer/robosoccer.ino
+++ robosoccer/robosoccer.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-const int en1 = 5, en2 = 6, in1 = 4, in2 = 7, in3 = 8, in4 = 9, statePin = 4;
+const int en1 = 5, en2 = 6, in1 = 4, in2 = 7, in3 = 8, in4 = 9, statePin = A0;
 char c = ' ';
 SoftwareSerial myBT(0, 1);  //0 as rx and 1 ax tx
 
@@ -19,49 +19,52 @@ void setup() {
 void loop() {
   digitalWrite(en1, HIGH);
   digitalWrite(en2, HIGH);
-  if (myBT.available() > 0) {
-    c = myBT.read();
-    switch (c) {
-      case 'F':
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        break;
-
-      case 'L':
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        break;
-
-      case 'R':
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        break;
-
-      case 'B':
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        break;
-
-      case 'S':
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        break;
-    }
-  }
-  if (statePin == LOW) {
+  if (analogRead(statePin) == 0) {
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
+  }
+  else
+  {
+    if (myBT.available() > 0) {
+      c = myBT.read();
+      switch (c) {
+        case 'F':
+          digitalWrite(in1, HIGH);
+          digitalWrite(in2, LOW);
+          digitalWrite(in3, HIGH);
+          digitalWrite(in4, LOW);
+          break;
+
+        case 'L':
+          digitalWrite(in1, LOW);
+          digitalWrite(in2, HIGH);
+          digitalWrite(in3, HIGH);
+          digitalWrite(in4, LOW);
+          break;
+
+        case 'R':
+          digitalWrite(in1, HIGH);
+          digitalWrite(in2, LOW);
+          digitalWrite(in3, LOW);
+          digitalWrite(in4, HIGH);
+          break;
+
+        case 'B':
+          digitalWrite(in1, LOW);
+          digitalWrite(in2, HIGH);
+          digitalWrite(in3, LOW);
+          digitalWrite(in4, HIGH);
+          break;
+
+        case 'S':
+          digitalWrite(in1, LOW);
+          digitalWrite(in2, LOW);
+          digitalWrite(in3, LOW);
+          digitalWrite(in4, LOW);
+          break;
+      }
+    }
   }
 }
